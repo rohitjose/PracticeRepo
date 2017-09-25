d6d9e62830447a319357121c3fc11da884170413
Rohit Jose <rohitjose@gmail.com>
2017-09-25 20:20:23 +1000

    Default parameters in a function

---
+++ ES6/defaultParameters.js
@@ -0,0 +1,17 @@
+/*
+ * Programming Quiz: Using Default Function Parameters (2-2)
+ */
+
+let buildHouse = ({
+	floors = 1,
+	color = 'red',
+	walls = 'brick'
+} = {}) => `Your house has ${floors} floor(s) with ${color} ${walls} walls.`;
+
+//tests
+console.log(buildHouse()); // Your house has 1 floor(s) with red brick walls.
+console.log(buildHouse({})); // Your house has 1 floor(s) with red brick walls.
+console.log(buildHouse({
+	floors: 3,
+	color: 'yellow'
+})); // Your house has 3 floor(s) with yellow brick walls.
