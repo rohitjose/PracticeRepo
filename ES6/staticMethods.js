4bd2099a40bffa7eb2dbb8a1449178a7935ebeec
Rohit Jose <rohitjose@gmail.com>
2017-09-25 21:08:08 +1000

    Subclass examples

---
+++ ES6/staticMethods.js
@@ -0,0 +1,17 @@
+class Plane {
+  constructor(numEngines) {
+    this.numEngines = numEngines;
+    this.enginesActive = false;
+  }
+
+  static badWeather(planes) {
+    for (plane of planes) {
+      plane.enginesActive = false;
+    }
+  }
+
+  startEngines() {
+    console.log('starting engines…');
+    this.enginesActive = true;
+  }
+}
