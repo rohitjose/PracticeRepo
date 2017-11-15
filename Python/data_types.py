3b653d63e469f5aaeb2226f16c1f9548086b7010
Rohit Jose <rohitjose@gmail.com>
2017-11-15 17:15:40 +1100

    Python program 2

---
+++ Python/data_types.py
@@ -0,0 +1,16 @@
+
+# Declare second integer, double, and String variables.
+second_int = 4
+second_double = 4.0
+second_string = 'HackerRank'
+# Read and save an integer, double, and String to your variables.
+i = input()
+d = input()
+s = input()
+# Print the sum of both integer variables on a new line.
+print("%d"%(second_int+int(i)))
+# Print the sum of the double variables on a new line.
+print("%.1f"%(second_double+float(d)))
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(second_string,s)
